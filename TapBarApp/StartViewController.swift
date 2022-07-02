3cc6b97469801103f69446a76b87ad22628cad1f
Sergejs Tiselskis <slokas85@gmail.con>
2022-07-02 18:32:48 +0300

    version 1.2

--- TapBarApp/StartViewController.swift
+++ TapBarApp/StartViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class StartViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let tabBarVC = segue.destination as? UITabBarController else { return }
+        guard let viewControllers = tabBarVC.viewControllers else { return }
+        
+        viewControllers.forEach { viewController in
+            if let firstVC = viewController as? FirstViewController {
+                firstVC.view.backgroundColor = .systemOrange
+            } else if let secondVC = viewController as? SecondViewController {
+                secondVC.view.backgroundColor = .systemMint
+            }
+        }
     }
-
 
 }
 
